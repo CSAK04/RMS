a69d8d19f45d41a06bb65f5ebde9cdd35050285d
CSAK04 <pgmak9975@gmail.com>
2023-09-27 21:19:18 +0530

    added comments for better understanding of code

--- gui.py
+++ gui.py
@@ -2,8 +2,11 @@ import tkinter as t
 import employee as e
 
 a = t.Tk()
+#code to insert title
 a.title('RESTAURANT MANAGEMENT SYSTEM')
+#code to insert icon
 a.iconbitmap('images/logo.ico')
+#code to give size of the form
 a.geometry('900x600')
 
 
